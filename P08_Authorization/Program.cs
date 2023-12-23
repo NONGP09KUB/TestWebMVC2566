25beaace006af207e012ea6194daf3dfaf48a957
NONGP09KUB <145835713+NONGP09KUB@users.noreply.github.com>
2023-12-23 10:33:39 +0700

    P08_การเริ่มต้นขั้นที่ 1 : Scaffold Identity Frame work ตัวจัดการ User

---
+++ P08_Authorization/Program.cs
@@ -0,0 +1,48 @@
+global
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using P08_Authorization.Areas.Identity.Data;
+using P08_Authorization.Data;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("AuthorizationContextConnection") ?? throw new InvalidOperationException("Connection string 'AuthorizationContextConnection' not found.");
+
+builder.Services.AddDbContext<AuthorizationContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<MyUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 3;
+
+}).AddEntityFrameworkStores<AuthorizationContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+// ต้องใส่ทุกครั้ง
+app.UseAuthentication();
+// ต้องใส่ทุกครั้ง
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+// ต้องใส่ทุกครั้ง
+app.MapRazorPages();
+// ต้องใส่ทุกครั้ง
+
+app.Run();
