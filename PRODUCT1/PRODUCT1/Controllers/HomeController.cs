c1c963e25da2147c0fb5f7326451ff4fe96b05c1
NONGP09KUB <145835713+NONGP09KUB@users.noreply.github.com>
2024-01-27 10:52:49 +0700

    HomeProduct หน้า Detail กับ หน้าสินค้า

--- PRODUCT1/PRODUCT1/Controllers/HomeController.cs
+++ PRODUCT1/PRODUCT1/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PRODUCT1.Data;
 using System.Diagnostics;
 
 namespace PRODUCT1.Controllers
@@ -6,21 +7,35 @@ namespace PRODUCT1.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ProductContext _productContext;
+        public HomeController(ILogger<HomeController> logger, ProductContext productContext)
         {
             _logger = logger;
+            _productContext = productContext;
         }
 
         public IActionResult Index()
         {
+            var product = _productContext.Products.ToList();
+            foreach (var item in product)
+            {
+                if (!string.IsNullOrEmpty(item.ImageUrl))
+                {
+                    item.ImageUrl = SD.ProductPath + "\\" + item.ImageUrl;
+                }
+
+            }
+            return View(product);
+        }
+
+        public IActionResult Detail(int productId)
+        {
+            var product = _productContext.Products.ToList();
+
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
